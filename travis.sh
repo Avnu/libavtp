80acba5cb6d432e39da69cba90f28cb203f3fc71
Ederson de Souza <ederson.desouza@intel.com>
2019-02-04 14:58:07 -0800

    examples: IEC 61883/IIDC MPEG-TS talker example
    This patch adds the examples/ieciidc-talker.c file that provides an
    example of how to use libavtp IEC 61883/IIDC APIs. The example implements
    a very simple talker, that reads MPEG-TS from standard input and creates
    and send AVTP IEC 61883/IIDC packets via network.
    
    The easiest way to use this example is to combine it with a GStreamer
    pipeline that generates MPEG-TS data and send to stdout.
    
    The ieciidc-talker.c file has an example of such pipeline.
    
    Signed-off-by: Ederson de Souza <ederson.desouza@intel.com>

--- travis.sh
+++ travis.sh
@@ -3,4 +3,12 @@ set -ev
 
 mkdir build
 CFLAGS=-Wno-missing-braces meson . build
-ninja -C build/ test aaf-talker aaf-listener crf-talker crf-listener cvf-talker cvf-listener
+ninja -C build/ \
+	test \
+	aaf-talker \
+	aaf-listener \
+	crf-talker \
+	crf-listener \
+	cvf-talker \
+	cvf-listener \
+	ieciidc-talker
